ba3fb94bc860f083149fbe313ee86fc0f8498e4f
Oleksandr Knyga <oleksandrknyga@gmail.com>
2015-01-14 03:30:46 -0800

    fixes

--- vkapi.js
+++ vkapi.js
@@ -1,63 +1,86 @@
 define(['jquery', 'underscore'], function($, _) {
 
-    var VKAPI = {};
-    VKAPI.url = 'https://api.vk.com/method';
-    VKAPI.version = '5.27';
-    VKAPI.token = '';
+    if(!window.VKAPI) {
+        var VKAPI = {};
+        VKAPI.attributes = {};
+        VKAPI.defaults = {
+            url: 'https://api.vk.com/method/',
+            version: '5.27',
+            token: ''
+        };
 
-    VKAPI._listeners = {};
-    VKAPI._scopes = {};
-    VKAPI.on = function(name, fnc, scope) {
-        if(!_.isArray(this._listeners[name])) {
-            this._listeners[name] = [];
-            this._scopes[name] = [];
-        }
+        VKAPI.get = function(name) {
+            var value = this.attributes[name];
 
-        this._listeners[name].push(fnc);
-        this._scopes[name].push(scope);
-    };
-    VKAPI.trigger = function(name) {
-        if(_.isArray(this._listeners[name])) {
-            var args = arguments.splice(1);
-            for(var i=0; i<this._listeners[name].length; i++) {
-                var fnc = this._listeners[name][i],
-                    scope = this._scopes[name][i];
-
-                fnc.apply(scope, args);
+            if(_.isUndefined(value)) {
+                value = this.defaults[name];
             }
-        }
-    };
 
-    VKAPI.fetch = function(ioptions, scope) {
-        if(_.isUndefined(scope)) {
-            scope = this;
-        }
+            return value;
+        };
 
-        var options = _.extend({}, {
-            callback: _.noop,
-            data: {},
-            action: '',
-            urlBase: VKAPI.url,
-            version: VKAPI.version,
-            token: VKAPI.token
-        });
+        VKAPI.set = function(name, value) {
+            this.attributes[name] = value;
+        };
 
-        $.ajax({
-            url: options.urlBase+options.action,
-            jsonp: 'callback',
-            data: _.extend(options.data, {
-                v: options.version,
-                access_token: options.token
-            }),
-            dataType: 'jsonp',
-            success: function(response) {
-                if(!_.isEmpty(response.error)) {
-                    VKAPI.trigger('error', response.error);
-                    return;
+        VKAPI._listeners = {};
+        VKAPI._scopes = {};
+        VKAPI.on = function(name, fnc, scope) {
+            if(!_.isArray(this._listeners[name])) {
+                this._listeners[name] = [];
+                this._scopes[name] = [];
+            }
+
+            this._listeners[name].push(fnc);
+            this._scopes[name].push(scope);
+        };
+        VKAPI.trigger = function(name) {
+            if(_.isArray(this._listeners[name])) {
+                var args = arguments.splice(1);
+                for(var i=0; i<this._listeners[name].length; i++) {
+                    var fnc = this._listeners[name][i],
+                        scope = this._scopes[name][i];
+
+                    fnc.apply(scope, args);
                 }
-
-                options.callback.apply(scope);
             }
-        });
+        };
+
+        VKAPI.exec = function(ioptions, scope) {
+            if(_.isUndefined(scope)) {
+                scope = this;
+            }
+
+            var options = _.extend({}, {
+                callback: _.noop,
+                data: {},
+                action: '',
+                urlBase: VKAPI.get('url'),
+                version: VKAPI.get('version'),
+                token: VKAPI.get('token')
+            }, ioptions);
+
+            $.ajax({
+                url: options.urlBase+options.action,
+                jsonp: 'callback',
+                data: _.extend(options.data, {
+                    v: options.version,
+                    access_token: options.token
+                }),
+                dataType: 'jsonp',
+                success: function(response) {
+                    if(!_.isEmpty(response.error)) {
+                        VKAPI.trigger('error', response.error);
+                        return;
+                    }
+
+                    options.callback.call(scope, response);
+                }
+            });
+        }
+
+        window.VKAPI = VKAPI;
     }
+
+    return window.VKAPI;
 });
